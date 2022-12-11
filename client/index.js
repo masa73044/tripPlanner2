9471bbdd23d247ee68354ed1512827393049e25b
masa73044 <masa7304@gmail.com>
2022-12-11 15:37:20 +0700

    start

---
+++ client/index.js
@@ -0,0 +1,17 @@
+import mapboxgl from "mapbox-gl";
+import buildMarker from "./marker.js";
+
+mapboxgl.accessToken = "YOUR API TOKEN HERE";
+
+const fullstackCoords = [-74.009, 40.705] // NY
+// const fullstackCoords = [-87.6320523, 41.8881084] // CHI
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: fullstackCoords, // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const marker = buildMarker("activities", fullstackCoords);
+marker.addTo(map);
